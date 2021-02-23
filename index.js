6f5b9264e23ee60a4ca57f7045b25e48da0c43c7
JS <66022264+Jsgithubchannel@users.noreply.github.com>
2021-02-23 12:05:39 +0900

    add: Add Media Query for Tablets on Section 1,2,3
    Add JS to section2 to remove pagination & extend cards for tablets

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const menu = document.querySelectorAll('.menu');
-const speed = 400;
+function menuAnchor() {
+  const menu = document.querySelectorAll(".menu");
+  const speed = 400;
 
-menu.forEach((item,index) => {
-    item.addEventListener('click', () => swiper.slideTo(index,speed));
-});
+  menu.forEach((item, index) => {
+    item.addEventListener("click", () => swiper.slideTo(index, speed));
+  });
+}
+
+function removePagination() {
+  if (window.screen.width >= 768) {
+    const mobile_container = document.querySelector("#for-mobile");
+    const pagination_v = mobile_container.querySelector(".swiper-pagination-v");
+    const cards = mobile_container.querySelectorAll(".cards");
+
+    mobile_container.classList.remove("swiper-container-v");
+    pagination_v.remove();
+    cards.forEach((item) => item.classList.remove("swiper-slide"));
+  }
+}
+
+function init() {
+  removePagination();
+  menuAnchor();
+}
+
+init();
